6da6de54cda7cdc1b5494d0ded4271885bfdb52b
Jasmina Bajramovic <jasmina@Jasminas-MacBook-Pro.local>
2017-11-18 14:47:57 +0100

    home panel-initial

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import App from './App';
 import Login from './Login';
 import Register from './Register';
 import Dashboard from './Dashboard';
+import Home from './Home';
 
 /* Render + router */
 ReactDOM.render (
@@ -20,7 +21,8 @@ ReactDOM.render (
             <Route path="/login" component={Login}/>
             <Route path="/register" component={Register}/>
             <Route path="/dashboard" component={Dashboard}/>
+            <Route path="/home" component={Home}/>
         </div>
-    </BrowserRouter>, 
+    </BrowserRouter>,
     document.getElementById('root')
 );
